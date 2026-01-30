3bc9d1369536b437c04f8ec1a44a1809a0882831
wu <wu@.com>
2026-01-30 10:21:02 +0800

    Logs-filips123/contenthashpy

--- setup.py
+++ setup.py
@@ -1,96 +1,63 @@
-#!/usr/bin/env python
+from setuptools import setup
+from pathlib import Path
 import os
-import re
-import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-def get_version(*file_paths):
-    """Retrieves the version from rest_framework_tus/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-version = get_version("rest_framework_tus", "__init__.py")
-
-
-if sys.argv[-1] == "publish":
-    try:
-        import wheel
-
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
-    sys.exit()
-
-if sys.argv[-1] == "tag":
-    print("Tagging the version on git:")
-    os.system(f"git tag -a {version} -m 'version {version}'")
-    os.system("git push --tags")
-    sys.exit()
-
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+# 安全读取 README.md，不区分大小写
+def readme():
+    root = Path(__file__).parent
+    readme_file = None
+    for name in os.listdir(root):
+        if name.lower() == "readme.md":
+            readme_file = root / name
+            break
+    if readme_file and readme_file.exists():
+        return readme_file.read_text(encoding="utf-8")
+    return ""  # 文件不存在时返回空字符串
 
 setup(
-    name="drf-tus",
-    version=version,
-    description="A Tus (tus.io) library for Django Rest Framework",
-    long_description=f"{readme}\n\n{history}",
-    author="Dirk Moors",
-    author_email="dirkmoors@gmail.com",
-    url="https://github.com/dirkmoors/drf-tus",
-    packages=[
-        "rest_framework_tus",
-    ],
-    include_package_data=True,
+    name='content-hash',
+    description='Python implementation of EIP 1577 content hash',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    version='1.0.0',
+
+    packages=['content_hash'],
+
+    entry_points={
+        'console_scripts': ['content-hash=content_hash.__main__:main'],
+    },
+
     install_requires=[
-        "python-dateutil>=2.8.2",
-        "Django>=3.2,<=4.2",
-        "djangorestframework>=3.14.0",
-        "jsonfield>=2.0.0",
-        "django-fsm==2.8.1",
-        "six>=1.11.0",
+        'py-cid>=0.3.0,<0.4.0',
+        'py-multicodec>=0.2.1,<0.3.0',
+        'py-multihash>=0.2.3,<0.3.0',
     ],
-    license="MIT",
-    zip_safe=False,
-    keywords=[
-        "drf-tus",
-        "tus",
-        "django",
-        "rest",
-        "framework",
-        "django-rest-framework",
-    ],
+
+    extras_require={
+        'lint': ['pylint'],
+        'test': ['pytest', 'pytest-cov'],
+    },
+
+    python_requires='>= 3.5',
+
+    author='Filip Š',
+    author_email='projects@filips.si',
+    url='https://github.com/filips123/ContentHashPy/',
+    keywords='ethereum, ethereum-name-service, ens, eip1577, web3, decentralized',
+
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Framework :: Django :: 4.1",
-        "Framework :: Django :: 4.2",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
     ],
+
+    include_package_data=True,
 )
