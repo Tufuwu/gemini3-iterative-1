b9b3ec66e203a97cb447def3c034ba9b27cf21fa
wu <wu@.com>
2026-01-30 10:28:40 +0800

    Logs-raphaelvallat/yasa

--- setup.py
+++ setup.py
@@ -1,39 +1,75 @@
-from setuptools import setup
+#! /usr/bin/env python
+#
+# Copyright (C) 2018 Raphael Vallat
 
-version = '1.0.0-dev'
-description = 'Type system extensions for programs checked with the mypy type checker.'
-long_description = '''
-Mypy Extensions
-===============
+DESCRIPTION = "Yet Another Spindle Algorithm"
+LONG_DESCRIPTION = """YASA (Yet Another Spindle Algorithm) : fast and robust detection of spindles, slow-waves, and rapid eye movements from sleep EEG recordings..
+"""
 
-The "mypy_extensions" module defines extensions to the standard "typing" module
-that are supported by the mypy type checker and the mypyc compiler.
-'''.lstrip()
+DISTNAME = 'yasa'
+MAINTAINER = 'Raphael Vallat'
+MAINTAINER_EMAIL = 'raphaelvallat9@gmail.com'
+URL = 'https://github.com/raphaelvallat/yasa/'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/raphaelvallat/yasa/'
+VERSION = '0.6.0'
+PACKAGE_DATA = {'yasa.data.icons': ['*.svg']}
 
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
+INSTALL_REQUIRES = [
+    'numpy',
+    'scipy',
+    'pandas',
+    'matplotlib',
+    'seaborn',
+    'mne>=0.20.0',
+    'numba',
+    'outdated',
+    'antropy',
+    'scikit-learn',
+    'tensorpac>=0.6.5',
+    'pyriemann>=0.2.7',
+    'lspopt',
+    'ipywidgets',
+    'joblib'
+]
+
+PACKAGES = [
+    'yasa',
+]
+
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Topic :: Software Development',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS'
 ]
 
-setup(
-    name='mypy_extensions',
-    python_requires='>=3.7',
-    version=version,
-    description=description,
-    long_description=long_description,
-    author='The mypy developers',
-    author_email='jukka.lehtosalo@iki.fi',
-    url='https://github.com/python/mypy_extensions',
-    license='MIT License',
-    py_modules=['mypy_extensions'],
-    classifiers=classifiers,
-)
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=INSTALL_REQUIRES,
+          include_package_data=True,
+          packages=PACKAGES,
+          package_data=PACKAGE_DATA,
+          classifiers=CLASSIFIERS,
+          )
