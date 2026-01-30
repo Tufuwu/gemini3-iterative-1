aa960a83fa3ab75cd202afe4b4b099d12185b1e7
wu <wu@.com>
2026-01-30 10:30:46 +0800

    Logs-roverdotcom/django-inlinecss

--- setup.py
+++ setup.py
@@ -1,75 +1,137 @@
-#! /usr/bin/env python
-#
-# Copyright (C) 2018 Raphael Vallat
+import os
+import sys
+from shutil import rmtree
 
-DESCRIPTION = "Yet Another Spindle Algorithm"
-LONG_DESCRIPTION = """YASA (Yet Another Spindle Algorithm) : fast and robust detection of spindles, slow-waves, and rapid eye movements from sleep EEG recordings..
-"""
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
 
-DISTNAME = 'yasa'
-MAINTAINER = 'Raphael Vallat'
-MAINTAINER_EMAIL = 'raphaelvallat9@gmail.com'
-URL = 'https://github.com/raphaelvallat/yasa/'
-LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = 'https://github.com/raphaelvallat/yasa/'
-VERSION = '0.6.0'
-PACKAGE_DATA = {'yasa.data.icons': ['*.svg']}
+# Package meta-data.
+NAME = 'django-inlinecss'
+SRC_DIR = 'django_inlinecss'
+DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
+URL = 'https://github.com/roverdotcom/django-inlinecss'
+EMAIL = 'philip@rover.com'
+AUTHOR = 'Philip Kimmey'
+REQUIRES_PYTHON = '>=3.8'
+VERSION = None
 
-INSTALL_REQUIRES = [
-    'numpy',
-    'scipy',
-    'pandas',
-    'matplotlib',
-    'seaborn',
-    'mne>=0.20.0',
-    'numba',
-    'outdated',
-    'antropy',
-    'scikit-learn',
-    'tensorpac>=0.6.5',
-    'pyriemann>=0.2.7',
-    'lspopt',
-    'ipywidgets',
-    'joblib'
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'Django>=3.2',
+    'pynliner',
+    'future>=0.16.0',
 ]
 
-PACKAGES = [
-    'yasa',
+# What packages are required only for tests?
+TESTS = [
+    'mock==5.1.0',
+    'pytest==8.0.1',
+    'pytest-django==4.8.0',
 ]
 
-CLASSIFIERS = [
-    'Intended Audience :: Science/Research',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: POSIX',
-    'Operating System :: Unix',
-    'Operating System :: MacOS'
-]
+# What packages are optional?
+EXTRAS = {
+    'flake8': [
+        'flake8==7.0.0',
+        'flake8-isort==6.1.1',
+        'isort==5.13.2',
+        'testfixtures==8.0.0',
+    ],
+    "tests": TESTS,
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 
 try:
-    from setuptools import setup
-    _has_setuptools = True
-except ImportError:
-    from distutils.core import setup
+    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
-if __name__ == "__main__":
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
 
-    setup(name=DISTNAME,
-          author=MAINTAINER,
-          author_email=MAINTAINER_EMAIL,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          install_requires=INSTALL_REQUIRES,
-          include_package_data=True,
-          packages=PACKAGES,
-          package_data=PACKAGE_DATA,
-          classifiers=CLASSIFIERS,
-          )
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print(f"\033[1m{s}\033[0m")
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    license="MIT",
+    url=URL,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    keywords=["html", "css", "inline", "style", "email"],
+    classifiers=[
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Email",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    install_requires=REQUIRED,
+    tests_require=TESTS,
+    extras_require=EXTRAS,
+    # $ setup.py upload support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
+)
